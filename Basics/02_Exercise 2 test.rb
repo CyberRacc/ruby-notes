6a93f31bda2e518abf90164c7cedb90b8daa2fed
CyberRacc <89751580+CyberRacc@users.noreply.github.com>
2023-07-16 23:53:28 +0100

    Finished notes on the basics of Ruby.

---
+++ Basics/02_Exercise 2 test.rb
@@ -0,0 +1,7 @@
+movies = {  :"Ready Player One" => 2018,
+    :"Avatar" => 2009,
+    :"Tron: Legacy" => 2010 }
+
+puts movies[:"Ready Player One"]
+puts movies[:"Avatar"]
+puts movies[:"Tron: Legacy"]
